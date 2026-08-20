a30fd8ba9861b2e6a9ce181c04c9aa38346091c8
Andrelino Daniel Filho <107147363+andrelinodaniel@users.noreply.github.com>
2026-08-20 16:41:35 -0400

    refactor: impleta arquitetura mvc separando rotas e controllers

---
+++ models/taskModel.js
@@ -0,0 +1,6 @@
+const tasks = [
+    { id: 1, title: 'Estudar Node.js', completed: true },
+    { id: 2, title: 'Criar a rota de tarefas', completed: false }
+];
+
+module.exports = tasks;
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const taskRoutes = require('./routes/taskRoutes');
+app.use('/tasks',taskRoutes);
 const port = 3000
-const tasks = [
-    { id: 1, title: 'Estudar Node.js', completed: true },
-    { id: 2, title: 'Criar a rota de tarefas', completed: false }
-];
 
 app.get('/', (req,res) => {
     res.json({
@@ -14,46 +12,6 @@ app.get('/', (req,res) => {
     })
 
 });
-
-app.get('/tasks', (req,res)=>{
-    return res.json(tasks.length == 0? {mensagem:"LISTA ESTÁ VAZIA!"}:tasks); 
-
-})
-
-app.post('/tasks', (req,res)=>{
-    console.log(req.body.title);
-    if (!req.body.title) {
-        return res.status(400).json({erro: 'O título é obrigatorio'})
-    }
-    const tarefa = {id:(tasks.length)+1,title:req.body.title,completed:false}
-    tasks.push(tarefa)
-    res.status(201).json(tarefa)
-   
-})
-
-app.put('/tasks/:id', (req,res)=>{
-    const idUrl = Number(req.params.id);
-    const posicao = tasks.findIndex(task => task.id == idUrl)
-    if (posicao === -1 ) return res.status(404).json({mensagem: "item não encontrado!" });
-    if (req.body.title !== undefined){
-        tasks[posicao].title = req.body.title;
-    }
-    if (req.body.completed !== undefined){
-         tasks[posicao].completed = req.body.completed;
-    }
-   
-    res.status(200).json(tasks[posicao]);
-
-
-})
-
-app.delete('/tasks/:id', (req,res) => {
-    const idUrl = Number(req.params.id);
-    const posicao = tasks.findIndex(task => task.id == idUrl )
-    if (posicao === -1 ) return res.status(404).json({mensagem: "item não encontrado!" });
-    const removedTask = tasks.splice(posicao,1)
-    res.status(200).json({msg:` Task ${removedTask[0].title} deletada!`})
-})
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}!`)
